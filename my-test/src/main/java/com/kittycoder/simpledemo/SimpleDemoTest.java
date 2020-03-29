bd3eb09d9d631f8373bf560b61ad9c42b97da255
lwei20000 <1291958264@qq.com>
2020-03-30 01:47:52 +0800

    target

--- my-test/src/main/java/com/kittycoder/simpledemo/SimpleDemoTest.java
+++ my-test/src/main/java/com/kittycoder/simpledemo/SimpleDemoTest.java
@@ -5,7 +5,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * 梁威测试文件
+ * 梁威测试文件(lw)
  */
 public class SimpleDemoTest {
 
@@ -13,5 +13,10 @@ public class SimpleDemoTest {
 		ApplicationContext ac = new ClassPathXmlApplicationContext("com/kittycoder/simpledemo/spring-student.xml");
 		Teacher teacher = (Teacher) ac.getBean("teacher");
 		System.out.println("=====================" + teacher);
+
+//		BeanFactory bf = new XmlBeanFactory(new ClassPathResource("beanFactoryTest.xml"));
+//		Teacher teacher = (Teacher) bf.getBean("teacher");
+//		System.out.println("=====================" + teacher);
+
 	}
 }
