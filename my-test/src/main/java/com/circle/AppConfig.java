27c1c498d17a2a2f90b714bb103a34b4f8c50ea6
lwei20000 <1291958264@qq.com>
2020-12-23 16:13:55 +0800

    循环依赖

---
+++ my-test/src/main/java/com/circle/AppConfig.java
@@ -0,0 +1,16 @@
+package com.circle;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * @Auther: weiliang
+ * @Date: 2020/12/23 15:28
+ * @Description:
+ */
+@Configuration
+@ComponentScan("com.circle.service")
+//@EnableAspectJAutoProxy
+public class AppConfig {
+}
---
+++ my-test/src/main/java/com/circle/service/AService.java
@@ -0,0 +1,15 @@
+package com.circle.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Auther: weiliang
+ * @Date: 2020/12/23 15:27
+ * @Description:
+ */
+@Component
+public class AService {
+	@Autowired
+	private BService bService;
+}
