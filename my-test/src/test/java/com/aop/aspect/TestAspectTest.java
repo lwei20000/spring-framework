ab17453c03dcf45137224f06b84243c8e5d88767
lwei20000 <1291958264@qq.com>
2020-12-25 22:42:14 +0800

    AOP

--- my-test/src/test/java/com/aop/aspect/TestAspectTest.java
+++ my-test/src/test/java/com/aop/aspect/TestAspectTest.java
@@ -13,7 +13,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath*:com/aop/aspect/applicationContext.xml"})
-public class TestAdvisorTest {
+public class TestAspectTest {
 
 	@Autowired
 	public TestPoint testPoint;
