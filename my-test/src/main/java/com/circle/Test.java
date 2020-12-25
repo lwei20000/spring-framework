21a416432eedd246a9dec5a7b2b612fb5553a019
lwei20000 <1291958264@qq.com>
2020-12-25 14:02:22 +0800

    循环依赖

--- my-test/src/main/java/com/circle/Test.java
+++ my-test/src/main/java/com/circle/Test.java
@@ -11,6 +11,6 @@ public class Test {
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 		System.out.println(ac.getBean("aService"));
-		System.out.println(ac.getBean("bService"));
+		//System.out.println(ac.getBean("bService"));
 	}
 }
