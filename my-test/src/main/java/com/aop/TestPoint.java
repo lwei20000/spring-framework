b8c61b361d1353f35135217d4eb59afd8aac4f23
lwei20000 <1291958264@qq.com>
2020-12-25 11:45:40 +0800

    循环依赖

--- my-test/src/main/java/com/aop/TestPoint.java
+++ my-test/src/main/java/com/aop/TestPoint.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Component;
 
 /**
  * 目标对象，需要被代理的类及方法
+ * 案例来源：https://www.cnblogs.com/yulinfeng/p/7841167.html
+ *
  * Created by Kevin on 2017/11/15.
  */
 @Component("testPoint")
