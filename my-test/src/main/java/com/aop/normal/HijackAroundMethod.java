9d950722eeaeae1a93990dc17c6163a24a6e4594
lwei20000 <1291958264@qq.com>
2020-12-26 18:39:53 +0800

    AOP

---
+++ my-test/src/main/java/com/aop/normal/HijackAroundMethod.java
@@ -0,0 +1,26 @@
+package com.aop.normal;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+import java.util.Arrays;
+
+/**
+ * @Auther: weiliang
+ * @Date: 2020/12/26 12:17
+ * @Description:
+ */
+public class HijackAroundMethod implements MethodInterceptor {
+		@Override
+		public Object invoke(MethodInvocation methodInvocation) throws Throwable {
+
+//			System.out.println("Method name : " + methodInvocation.getMethod().getName());
+//			System.out.println("Method arguments : " + Arrays.toString(methodInvocation.getArguments()));
+
+			System.out.println("HijackAroundMethod : Before method hijacked!");
+			Object result = methodInvocation.proceed();
+			System.out.println("HijackAroundMethod : After method hijacked!");
+			return result;
+
+		}
+	}
