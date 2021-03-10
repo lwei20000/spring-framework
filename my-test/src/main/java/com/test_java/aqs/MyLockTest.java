67f4fabd77398bbdb225ee1a91148f0f98a62399
lwei20000 <1291958264@qq.com>
2021-03-10 22:57:48 +0800

    清理

---
+++ my-test/src/main/java/com/test_java/aqs/MyLockTest.java
@@ -0,0 +1,65 @@
+package com.test_java.aqs;
+
+import java.util.concurrent.locks.Lock;
+
+/**
+ *
+ */
+public class MyLockTest {
+
+	private int num = 0;
+	private int aNum = 0;
+
+	private Lock lock = new MyLock();
+
+	public void addNum() {
+		lock.lock();
+		try {
+			num++;
+			// 调用另外一个需要锁的方法，测试是否能重入
+
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public void a1() {
+		lock.lock();
+		try{
+			aNum++;
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public static void main(String[] args) {
+		MyLockTest t = new MyLockTest();
+
+		for (int i = 0; i < 10; i++) {
+			new Thread(() -> {
+				for (int j = 0; j < 100; j++) {
+					t.addNum();
+					try {
+						Thread.sleep(1L);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}).start();
+		}
+
+		for (int i = 0; i < 11; i++) {
+			System.out.println("now num====" + t.getNum());
+			try{
+				Thread.sleep(100L);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
