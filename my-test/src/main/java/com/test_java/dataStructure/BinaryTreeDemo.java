6e72ed171d51af89f3467d452fc9f3252631af30
“lw20000” <1291958264@qq.com>
2021-03-20 21:08:24 +0800

    清理

--- my-test/src/main/java/com/test_java/dataStructure/BinaryTreeDemo.java
+++ my-test/src/main/java/com/test_java/dataStructure/BinaryTreeDemo.java
@@ -11,18 +11,39 @@ public class BinaryTreeDemo {
 		HeroNode node2 = new HeroNode(2,"吴用");
 		HeroNode node3 = new HeroNode(3,"卢俊义");
 		HeroNode node4 = new HeroNode(4,"林冲");
+		HeroNode node5 = new HeroNode(5,"关胜");
+
 
 		// 说明：我们先手动创建该二叉树；后面学习通过递归方法创建二叉树
 		root.setLeft(node2);
 		root.setRight(node3);
+		node3.setLeft(node5);
 		node3.setRight(node4);
 		binaryTree.setRoot(root);
 
-		// 测试
-		System.out.println("前序遍历"); // 1，2，3，4
+		// 测试--遍历
+		System.out.println("前序遍历");
+		binaryTree.preOrder();  // 1，2，3，4
+		System.out.println("中序遍历");
+		binaryTree.infixOrder();// 2，1，3，4
+		System.out.println("后序遍历");
+		binaryTree.postOrder(); // 2，4，3，1
+
+		// 测试--查找
+		System.out.println("前序查找");
+		HeroNode resNode = binaryTree.preOrderSearch(5);
+		if(resNode != null) {
+			System.out.println("找到了：" + resNode.toString());
+		} else {
+			System.out.println("没有找到no=" + resNode.getNo() + "的节点");
+		}
+
+		// 测试--删除
+		System.out.println("删除前，前序遍历");
 		binaryTree.preOrder();
-
-
+		binaryTree.delNode(5);
+		System.out.println("删除后，前序遍历");
+		binaryTree.preOrder();
 
 	}
 }
@@ -34,6 +55,7 @@ class BinaryTree {
 		this.root = root;
 	}
 
+	/******* 遍历 *********/
 	// 前序遍历
 	public void preOrder() {
 		if(this.root != null) {
@@ -58,6 +80,47 @@ class BinaryTree {
 			this.root.postOrder();
 		} else {
 			System.out.println("当前二叉树为空，无法遍历！");
+		}
+	}
+
+	/******* 查找 *********/
+	public HeroNode preOrderSearch(int no) {
+		if(root != null) {
+			return root.preOrderSearch(no);
+		} else {
+			return null;
+		}
+	}
+
+	public HeroNode infixOrderSearch(int no) {
+		if(root != null) {
+			return root.infixOrderSearch(no);
+		} else {
+			return null;
+		}
+	}
+
+	public HeroNode postOrderSearch(int no) {
+		if(root != null) {
+			return root.preOrderSearch(no);
+		} else {
+			return null;
+		}
+	}
+
+	/******* 删除 *********/
+	public void delNode(int no) {
+		if(root != null) {
+			// 如果只有一个root节点，这里就需要立即判读是否root就是要删除的节点
+			if(root.getNo() == no) {
+				root = null;
+			} else {
+				// 递归删除
+				root.delNode(no);
+			}
+
+		} else {
+			System.out.println("空树，无法删除");
 		}
 	}
 }
@@ -151,5 +214,137 @@ class HeroNode{
 		}
 		System.out.println(this); // 输出父节点
 	}
+
+	// 前序查找
+	public HeroNode preOrderSearch(int no) {
+		// 比较当前节点是不是
+		if(this.no == no) {
+			return this;
+		}
+
+		// 1 则判断当前节点的左节点是否为空，如果不为空，则递归前序查找
+		// 2 如果左递归前序查找，找到节点，则返回
+		HeroNode resNode = null;  // 记录找到的节点
+		if(this.left != null) {
+			resNode = this.left.preOrderSearch(no);
+		}
+		if(resNode != null) {
+			return resNode;
+		}
+
+		// 1 左递归前序查找，找到节点，则返回，否则继续判断。
+		// 2 当前的节点的右子节点是否为空，如果不为空，则继续向右递归前序查找。
+		if(this.right != null) {
+			resNode = this.right.preOrderSearch(no);
+		}
+		return resNode;
+	}
+
+	// 中序查找
+	public HeroNode infixOrderSearch(int no) {
+		// 1 则判断当前节点的左节点是否为空，如果不为空，则递归前序查找
+		// 2 如果左递归前序查找，找到节点，则返回
+		HeroNode resNode = null;  // 记录找到的节点
+		if(this.left != null) {
+			resNode = this.left.infixOrderSearch(no);
+		}
+		if(resNode != null) {
+			return resNode;
+		}
+
+		// 比较当前节点是不是
+		if(this.no == no) {
+			return this;
+		}
+
+		// 1 左递归前序查找，找到节点，则返回，否则继续判断。
+		// 2 当前的节点的右子节点是否为空，如果不为空，则继续向右递归前序查找。
+		if(this.right != null) {
+			resNode = this.right.infixOrderSearch(no);
+		}
+		return resNode;
+	}
+
+	// 中序查找
+	public HeroNode postOrderSearch(int no) {
+		// 1 则判断当前节点的左节点是否为空，如果不为空，则递归前序查找
+		// 2 如果左递归前序查找，找到节点，则返回
+		HeroNode resNode = null;  // 记录找到的节点
+		if(this.left != null) {
+			resNode = this.left.postOrderSearch(no);
+		}
+		if(resNode != null) {
+			return resNode;
+		}
+
+		// 1 左递归前序查找，找到节点，则返回，否则继续判断。
+		// 2 当前的节点的右子节点是否为空，如果不为空，则继续向右递归前序查找。
+		if(this.right != null) {
+			resNode = this.right.postOrderSearch(no);
+		}
+		if(resNode != null) {
+			return resNode;
+		}
+
+		// 比较当前节点是不是
+		if(this.no == no) {
+			return this;
+		}
+
+		return resNode;
+	}
+
+	// 递归删除节点：
+	// 1 如果删除的是叶子节点，直接删除。
+	// 2 如果删除的不是叶子节点，删除其子树
+	public void delNode(int no) {
+		// 2 如果当前节点的左子树不为空，并且左子节点就是要删除的节点，就将this.left = null;并且返回
+		if(this.left != null && this.left.no == no) {
+			this.left = null;
+			return;
+		}
+		// 3 如果当前节点的右子树不为空，并且右子节点就是要删除的节点，就将this.right = null;并且返回
+		if(this.right != null && this.right.no == no) {
+			this.right = null;
+			return;
+		}
+
+		// 4 我们就需要向左子树进行递归删除
+		if(this.left != null) {
+			this.left.delNode(no);
+		}
+
+		// 5 我们就需要向右子树进行递归删除
+		if(this.right != null) {
+			this.right.delNode(no);
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
---
+++ my-test/src/main/java/com/test_java/dataStructure/readme.md
@@ -0,0 +1,64 @@
+## 二叉树
+
+
+
+### 查找
+
+<img src="https://cdn.jsdelivr.net/gh/lwei20000/pic/image-20210320195316986.png" alt="image-20210320195316986" style="zoom:50%;" />
+
+### 删除
+
+<img src="https://cdn.jsdelivr.net/gh/lwei20000/pic/image-20210320203200228.png" alt="image-20210320203200228" style="zoom: 33%;" />
+
+
+
+<img src="https://cdn.jsdelivr.net/gh/lwei20000/pic/image-20210320210524802.png" alt="image-20210320210524802" style="zoom:33%;" />
+
+> 注意：
+>
+> 1. 二叉树是单向的。
+
+### 顺序存储
+
+> 从数据存储来看，数组存储方式和树的存储方式可以相互转换，即数组可以转换成树，树也可以转换成数组。
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
